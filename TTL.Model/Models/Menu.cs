f37d0364b2e7c729dea127237d9ff072c5970427
trantronglu <tran.trong.lu@gmail.com>
2019-05-28 01:28:58 +0700

    Commit Data model

---
+++ TTL.Model/Models/Menu.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TTL.Model.Models
+{
+    [Table("Menus")]
+    public class Menu
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(256)]
+        public string URL { get; set; }
+
+        public int? DisplayOrder { get; set; }
+
+        [Required]
+        public string GroupID { set; get; }
+
+        [ForeignKey("GroupID")]
+        public virtual MenuGroup MenuGroup { set; get; }
+
+        [MaxLength(10)]
+        public string Target { set; get; }
+
+        public bool Status { set; get; }
+    }
+}
---
+++ TTL.Model/Models/Product.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
+using TTL.Model.Abstract;
+
+namespace TTL.Model.Models
+{
+    [Table("Products")]
+    public class Product : Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { set; get; }
+
+        [Required]
+        [MaxLength(256)]
+        public string Name { set; get; }
+
+        [Required]
+        [MaxLength(256)]
+        public string Alias { set; get; }
+
+        [Required]
+        public string CategoryID { set; get; }
+
+        [MaxLength(256)]
+        public string Image { set; get; }
+
+        [Column(TypeName = "xml")]
+        public XElement MoreImages { set; get; }
+
+        public decimal Price { set; get; }
+
+        public decimal? PromotionPrice { set; get; }
+
+        public int? Warranty { get; set; }
+
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+        public string Content { get; set; }
+
+        public bool? HomeFlag { get; set; }
+        public bool? HotFlag { get; set; }
+
+        public int? ViewsCount { set; get; }
+
+        public string Tags { set; get; }
+
+        [ForeignKey("CategoryID")]
+        public virtual ProductCategory ProductCategory { get; set; }
+    }
+}
